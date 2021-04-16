1e4dc8607f234eb1dcf5fba4558db9732c9f3a45
pguduguntla <praneethg347@gmail.com>
2021-04-16 16:33:38 -0500

    add logic to tweet day of week

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+import tweepy
+import datetime
+
+load_dotenv()
+consumer_key = os.getenv('API_KEY')
+consumer_secret = os.getenv('SECRET_KEY')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+def publictweet():
+    if datetime.date.today().weekday() == 0:
+        tweettopublish = 'Hi everyone, today is Monday.   #Monday '
+    if datetime.date.today().weekday() == 1:
+        tweettopublish = 'Enjoy your Tuesday.  #Tuesday'
+    if datetime.date.today().weekday() == 2:
+        tweettopublish = 'Third week of the Week. #Wednesday'
+    if datetime.date.today().weekday() == 3:
+        tweettopublish = 'Thursday. I cannot wait for the Weekend'
+    if datetime.date.today().weekday() == 4:
+        tweettopublish = 'Friday...Finally'
+    if datetime.date.today().weekday() == 5:
+        tweettopublish = 'Great it is Saturday #weekend #Saturday'
+    if datetime.date.today().weekday() == 6:
+        tweettopublish = 'Sunday morning...#Weekend #enjoy '
+        
+    api.update_status(tweettopublish)
+    print(tweettopublish)
+
+publictweet()
