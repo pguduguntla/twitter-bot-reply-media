7935ae992fa41189a4923f05e2df89c2ed71ff63
pguduguntla <praneethg347@gmail.com>
2021-04-18 11:39:45 -0500

    Add simple logging to bot

--- bot.js
+++ bot.js
@@ -4,17 +4,20 @@ const client = auth();
 
 // Call the stream function and pass in 'statuses/filter', our filter object, and our callback
 client.stream('statuses/filter', { track: '#RemoveNoise' }, function (stream) {
+  console.log("Searching for tweets...");
 
   // ... when we get tweet data...
   stream.on('data', function (tweet) {
-    console.log(tweet);
+    console.log("Found one!");
+    console.log("Recieved tweet reading...", tweet.text);
+
+    // check if tweet contains some media
     if (tweet.media_url) {
-      //call the post function to tweet reply video
-      console.log("has media")
-      postReplyWithMedia(client, "../video2.mp4", tweet);
+      console.log("Replying to tweet with video.");
+      postReplyWithMedia(client, "./sample-media/video.mp4", tweet);
       
     } else {
-      console.log("no media")
+      console.log("Tweet didn't provide media. Replying with message.");
       const message = "Oops, looks like you didn't provide a media file!";
       postReply(client, message, tweet);
     }
